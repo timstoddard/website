1563feaed9dacfcf90ec9277571a152f17dc720a
trackstar200 <tim.stoddard2@gmail.com>
2016-09-29 00:16:12 -0700

    minor tweaks

--- modules/NotFound.js
+++ modules/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default React.createClass({
   render() {
     document.title = 'Page Not Found';
-    return <div>
+    return <div className="notFound">
       <h1>The page you're looking for does not exist.</h1>
+      <h1><Link className="notFound__link" to="" key="home">Click here to go to the homepage.</Link></h1>
       <img src="https://http.cat/404" />
     </div>;
   }

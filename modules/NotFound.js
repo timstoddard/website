2932ad878cd8d0a98b00f40631d0fc29e7931063
trackstar200 <tim.stoddard2@gmail.com>
2016-09-29 00:10:47 -0700

    new 404 page

--- modules/NotFound.js
+++ modules/NotFound.js
@@ -3,6 +3,9 @@ import React from 'react';
 export default React.createClass({
   render() {
     document.title = 'Page Not Found';
-    return <div>The page you're looking for does not exist.</div>;
+    return <div>
+      <h1>The page you're looking for does not exist.</h1>
+      <img src="https://http.cat/404" />
+    </div>;
   }
 });

86f0a6d13c0b466336ac95b1f8a0b2ccbd05e954
trackstar200 <tim.stoddard2@gmail.com>
2016-10-03 07:06:36 -0700

    make dev webpack config way more swaggy

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'eval',
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    publicPath: ''
+    path: __dirname + '/dist',
+    publicPath: '/dist'
   },
   module: {
     loaders: [
@@ -13,9 +17,17 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader?presets[]=es2015&presets[]=react',
         exclude: /node_modules/
+      },
+      {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract('style-loader', ['css-loader', 'postcss-loader', 'sass-loader'])
       }
     ]
   },
+  postcss: function () {
+    return [autoprefixer];
+  },
+  plugins: [new ExtractTextPlugin('bundle.css', { allChunks: true})],
   devServer: {
     historyApiFallback: true,
   }

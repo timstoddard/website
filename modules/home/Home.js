d3781f27563ed62177c4a1aeeeae6cdeb5ff8784
trackstar200 <tim.stoddard2@gmail.com>
2016-10-09 02:37:19 -0700

    modified homepage link hover style

--- modules/home/Home.js
+++ modules/home/Home.js
@@ -3,25 +3,48 @@ import { Link } from 'react-router';
 
 import './Home.scss';
 
+let SplitLetters = React.createClass({
+  propTypes: {
+    text: React.PropTypes.string
+  },
+  render() {
+    let letters = this.props.text.split('').map((letter, index) =>
+      <span key={index} className="home__link--letter">{letter}</span>)
+    return <div>{letters}</div>;
+  }
+});
+
 export default React.createClass({
   render() {
     document.title = 'Tim Stoddard';
     return <div className="home">
       <div className="home__verticalAlignWrapper">
-        <h1 className="home__header--name">tim</h1>
-        <h1 className="home__header--name">stod</h1>
-        <h1 className="home__header--name">dard</h1>
+        <h1 className="home__header--name">
+          <Link to="">tim</Link>
+        </h1>
+        <h1 className="home__header--name">
+          <Link to="">stod</Link>
+        </h1>
+        <h1 className="home__header--name">
+          <Link to="">dard</Link>
+        </h1>
       </div>
       <div className="home__verticalAlignWrapper">
         <ul className="home__links center-align">
           <li className="home__link">
-            <Link className="home__link--text flow-text" to="/about">About Me</Link>
+            <Link className="home__link--text flow-text" to="/about">
+              <SplitLetters text="about" />
+            </Link>
           </li>
           <li className="home__link">
-            <Link className="home__link--text flow-text" to="/projects">Projects</Link>
+            <Link className="home__link--text flow-text" to="/projects">
+              <SplitLetters text="projects" />
+            </Link>
           </li>
           <li className="home__link">
-            <Link className="home__link--text flow-text" to="/resume">Resume</Link>
+            <Link className="home__link--text flow-text" to="/resume">
+              <SplitLetters text="resume" />
+            </Link>
           </li>
         </ul>
       </div>

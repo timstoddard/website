bcbf767b4d507c934da1deb5ce11d866db038431
trackstar200 <tim.stoddard2@gmail.com>
2016-10-08 18:06:19 -0700

    add gzipping to production server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+app.use(compression());
 
 // serve static assets normally
 app.use(express.static(__dirname));

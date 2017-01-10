c63be4e9ffc18805bcd7c01f2968378de188a350
trackstar200 <tim.stoddard2@gmail.com>
2017-01-09 20:51:14 -0800

    update about message

--- modules/home/About.js
+++ modules/home/About.js
@@ -44,7 +44,7 @@ export default React.createClass({
           I currently work as a software engineering intern at <a className="about__link" href="https://socreate.it" target="_blank">SoCreate</a>, where I use Angular 2 to build and refactor front-end components for the application they are working on. I also wrote a complete testing suite for the app using Jasmine. In addition to that, I have also worked on the backend api, which is written in PHP and uses MS SQL for databases.
         </p>
         <p className="about__text">
-          In my free time, I enjoy lifting weights, a passion that I have enjoyed since I started in 10th grade.
+          In my free time, I am learning ReactJS - in fact, this site uses React and React Router. I also enjoy lifting weights, a passion that I have enjoyed since I started in 10th grade.
         </p>
         <p className="about__text">
           Feel free to have a look around the site and/or <a className="about__link" href="mailto:tstoddar@calpoly.edu?subject=Saw%20your%20site,%20would%20like%20to%20get%20in%20touch%20with%20you">contact me</a> if you are interested in working together!

b898ab77cacdc1ccde30c752fee1c362d6b025ec
trackstar200 <tim.stoddard2@gmail.com>
2017-01-09 20:36:58 -0800

    update about page

--- modules/home/About.js
+++ modules/home/About.js
@@ -36,16 +36,19 @@ export default React.createClass({
     document.title = 'About';
     return <div className="about">
       <div className="container">
-        <h3 className="about__title center-align">Hi, I'm Tim!</h3>
-        <h5 className="about__text">
+        <h5 className="about__title center-align">Hi, I'm Tim!</h5>
+        <p className="about__text">
           I am a second year student at <a className="about__link" href="http://calpoly.edu" target="_blank">Cal Poly, San Luis Obispo</a> studying Computer Science.
-        </h5>
-        <h5 className="about__text">
-          I currently work as a software engineering intern at <a className="about__link" href="https://socreate.it" target="_blank">SoCreate</a>, where I use Angular 2 to build front-end components for the application they are working on. Over the past few months, I wrote and maintained Jasmine unit tests to cover the entire application — about 600 tests in total.
-        </h5>
-        <h5 className="about__text">
+        </p>
+        <p className="about__text">
+          I currently work as a software engineering intern at <a className="about__link" href="https://socreate.it" target="_blank">SoCreate</a>, where I use Angular 2 to build and refactor front-end components for the application they are working on. I also wrote a complete testing suite for the app using Jasmine. In addition to that, I have also worked on the backend api, which is written in PHP and uses MS SQL for databases.
+        </p>
+        <p className="about__text">
+          In my free time, I enjoy lifting weights, a passion that I have enjoyed since I started in 10th grade.
+        </p>
+        <p className="about__text">
           Feel free to have a look around the site and/or <a className="about__link" href="mailto:tstoddar@calpoly.edu?subject=Saw%20your%20site,%20would%20like%20to%20get%20in%20touch%20with%20you">contact me</a> if you are interested in working together!
-        </h5>
+        </p>
         <IconLinks />
         <Link className="" to=""><h5 className="about__link center-align">Home</h5></Link>
       </div>

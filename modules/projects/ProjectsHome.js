706f4dca476e1767e4835aa557869e3314bf1ef9
trackstar200 <tim.stoddard2@gmail.com>
2017-01-09 20:48:51 -0800

    update projects home page

--- modules/projects/ProjectsHome.js
+++ modules/projects/ProjectsHome.js
@@ -7,39 +7,36 @@ export default React.createClass({
   getInitialState() {
     return {
       data: [
-        { name: 'Car Crash Bingo', href: '/projects/bingo', works: true },
-        { name: 'Max-Heap Visualizer', href: '/projects/heap', works: true },
-        { name: 'Angular 2 Import Fixer', href: '/projects/imports', works: true },
-        { name: 'Strobe Light!', href: '/projects/strobe', works: false },
-        { name: 'Javascript Clock', href: '/projects/time', works: true },
-        { name: 'Zen Mode (Peaceful, Infinite Animation)', href: '/projects/zen', works: true }
+        { name: 'Car Crash Bingo', href: '/projects/bingo' },
+        { name: 'Max-Heap Visualizer', href: '/projects/heap' },
+        { name: 'Angular 2 (ES6) Import Fixer', href: '/projects/imports' },
+        { name: 'Strobe Light!', href: '/projects/strobe' },
+        { name: 'Javascript Clock', href: '/projects/time' },
+        { name: 'Zen Mode (Peaceful, Infinite Animation)', href: '/projects/zen' }
       ]
     }
   },
   render() {
     document.title = 'Projects';
     let links = this.state.data.map((link, index) => {
-      let modifier = link.works ? 'works' : 'broken';
       return <li key={index}>
         <Link to={link.href}>
-          <h5 className={`projectsHome__link projectsHome__link--${modifier}`}>
+          <h5 className="projectsHome__link projectsHome__link--project">
             {link.name}
           </h5>
         </Link>
       </li>;
     });
-    return <div className="projectsHome__background">
-      <div className="container">
-        <h5 className="projectsHome__title">Check out some of the cool things I've made!*</h5>
-        <p>*I'm still in the process of porting some of these projects from vanilla javascript into React. See the list below for which ones currently work.</p>
-        <ul>
-          {links}
-        </ul>
-        <hr className="projectsHome__divider" />
-        <div className="center-align">
-          <Link to="" className="projectsHome__link--home">Home</Link>
-        </div>
-      </div>
+    return <div className="container">
+      <h5 className="projectsHome__title">Check out some of the cool things I've made!</h5>
+      <p>Most of these projects were originally written in vanilla Javascript + jQuery, so I rewrote them in React to work with this site. Click on any project's name to view it!</p>
+      <ul>
+        {links}
+      </ul>
+      <hr className="projectsHome__divider" />
+      <h5 className="center-align">
+        <Link to="" className="projectsHome__link">Home</Link>
+      </h5>
     </div>;
   }
 });

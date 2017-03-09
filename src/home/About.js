28c83655f09418286414cd0cd55cde62f3e625fe
Tim Stoddard <tim.stoddard2@gmail.com>
2017-03-08 23:55:10 -0800

    redo about me page

--- src/home/About.js
+++ src/home/About.js
@@ -1,65 +1,63 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 
-let IconLinks = React.createClass({
-  getInitialState() {
-    return {
-      icons: [
-        { name: 'Github', href: 'https://github.com/timstoddard' },
-        { name: 'LinkedIn', href: 'https://linkedin.com/in/timstoddard200' },
-        { name: 'Portfolium', href: 'https://portfolium.com/timstoddard' },
-        { name: 'Facebook', href: 'https://facebook.com/timstoddard200' }
-      ]
-    }
-  },
-  render() {
-    return (
-      <div className="about__icons">
-        {this.state.icons.map((icon, index) => (
-          <a
-            href={icon.href}
-            target="_blank"
-            key={index}>
-            <img
-              src={`../../media/logos/${icon.name.toLowerCase()}.png`}
-              alt={icon.name}
-              className="about__icon" />
-          </a>)
-        )}
-      </div>
-    )
-  }
-})
+const IconLinks = ({ icons }) =>
+  <div className="about__icons">
+    {icons.map(icon =>
+      <a
+        href={icon.href}
+        target="_blank"
+        key={icon.name}>
+        <img
+          src={`../../media/logos/${icon.name.toLowerCase()}.png`}
+          alt={icon.name}
+          className="about__icon" />
+      </a>
+    )}
+  </div>
 
-export default React.createClass({
-  render() {
-    document.title = 'About'
-    return (
-      <div className="about">
-        <div className="container">
-          <h5 className="about__title center-align">
-            Hi, I'm Tim!
+IconLinks.propTypes = {
+  icons: PropTypes.array
+}
+
+IconLinks.defaultProps = {
+  icons: [
+    { name: 'Github', href: 'https://github.com/timstoddard' },
+    { name: 'LinkedIn', href: 'https://linkedin.com/in/timstoddard200' },
+    { name: 'Portfolium', href: 'https://portfolium.com/timstoddard' },
+    { name: 'Facebook', href: 'https://facebook.com/timstoddard200' }
+  ]
+}
+
+const About = () => {
+  document.title = 'About Me'
+  return (
+    <div className="about">
+      <div className="about__container container">
+        <h5 className="about__title center-align">
+          About Me
+        </h5>
+        <p className="about__text">
+          Hi, I'm Tim! I am a second year student at <a className="about__link" href="http://calpoly.edu" target="_blank">Cal Poly, San Luis Obispo</a> studying Computer Science.
+        </p>
+        <p className="about__text">
+          I am currently a software engineering intern at <a className="about__link" href="https://socreate.it" target="_blank">SoCreate</a>, where I regularly work with Angular 2, PHP, and MS SQL. Other technologies I've used there include Git, Jira, and BitBucket.
+        </p>
+        <p className="about__text">
+          In my free time, I am learning ReactJS - in fact, this site uses React and React Router. I also lift weights several times a week, a passion that I have enjoyed since my sophomore year of high school.
+        </p>
+        <p className="about__text">
+          Feel free to have a look around the site and/or <a className="about__link" href="mailto:tstoddar@calpoly.edu?subject=Saw%20your%20site%20and%20would%20like%20to%20get%20in%20touch%20with%20you!">contact me</a> if you are interested in working together!
+        </p>
+        <IconLinks />
+        <Link to="">
+          <h5 className="about__link center-align">
+            Back
           </h5>
-          <p className="about__text">
-            I am a second year student at <a className="about__link" href="http://calpoly.edu" target="_blank">Cal Poly, San Luis Obispo</a> studying Computer Science.
-          </p>
-          <p className="about__text">
-            I currently work as a software engineering intern at <a className="about__link" href="https://socreate.it" target="_blank">SoCreate</a>, where I use Angular 2 to build and refactor front-end components for the application they are working on. I also wrote a complete testing suite for the app using Jasmine. In addition to that, I have  worked on the backend api, which is written in PHP and uses MS SQL for storing data.
-          </p>
-          <p className="about__text">
-            In my free time, I am learning ReactJS - in fact, this site uses React and React Router. I also enjoy lifting weights, a passion that I have enjoyed since I started in 10th grade.
-          </p>
-          <p className="about__text">
-            Feel free to have a look around the site and/or <a className="about__link" href="mailto:tstoddar@calpoly.edu?subject=Saw%20your%20site,%20would%20like%20to%20get%20in%20touch%20with%20you">contact me</a> if you are interested in working together!
-          </p>
-          <IconLinks />
-          <Link to="">
-            <h5 className="about__link center-align">
-              Home
-            </h5>
-          </Link>
-        </div>
+        </Link>
       </div>
-    )
-  }
-})
+    </div>
+  )
+}
+
+export default About

100adb6020513f400242acc4921acd769ff5f7e4
Tim Stoddard <tim.stoddard2@gmail.com>
2017-12-12 22:45:23 -0800

    improve performance of home page dots

--- src/home/Dots.jsx
+++ src/home/Dots.jsx
@@ -4,37 +4,39 @@ export default class Dots extends Component {
   constructor(props) {
     super(props)
 
-    this.moveDots = this.moveDots.bind(this)
-
-    this.state = {
-      visible: false,
-      dots: this.initDots(),
-      pairs: [],
-      viewportWidth: 0,
-      viewportHeight: 0,
-    }
+    this.state = { visible: false }
   }
 
   componentDidMount() {
+    // timer to make dots visible
     setTimeout(() => {
       this.setState({ visible: true })
     }, this.props.delay)
-    this.moveInterval = setInterval(this.moveDots, 16)
-    window.onresize = this.saveViewportDimensions.bind(this)
+
+    // generate the dots
+    this.dots = []
+    for (let i = 0; i < 50; i++) {
+      this.dots.push(this.generateNewDot())
+    }
+
+    // start the interval to move the dots
+    this.moveInterval = setInterval(this.moveDots.bind(this), 5)
+
+    // track the window size
     this.saveViewportDimensions()
+    window.onresize = this.saveViewportDimensions.bind(this)
   }
 
-  initDots() {
-    const dots = []
-    for (let i = 0; i < 50; i++) {
-      dots.push(this.generateNewDot())
-    }
-    return dots
+  componentWillUnmount() {
+    clearInterval(this.moveInterval)
   }
 
   moveDots() {
-    const { viewportWidth, viewportHeight } = this.state
-    const dots = this.state.dots.map(({ x, y, dx, dy, radius, opacity }) => {
+    const {
+      clientWidth: viewportWidth,
+      clientHeight: viewportHeight,
+    } = document.documentElement
+    this.dots = this.dots.map(({ x, y, dx, dy, radius, opacity }) => {
       const newX = x + dx
       const newY = y + dy
       const outOfBounds = newX < 0 ||
@@ -45,33 +47,25 @@ export default class Dots extends Component {
          ? this.generateNewDot()
          : { x: newX, y: newY, dx, dy, radius, opacity }
     })
-    const pairs = []
-    for (let i = 0; i < dots.length; i++) {
-      for (let j = i + 1; j < dots.length; j++) {
-        const dot1 = dots[i]
-        const dot2 = dots[j]
-        const dist = Math.sqrt(Math.pow(dot1.x - dot2.x, 2) + Math.pow(dot1.y - dot2.y, 2))
+    const canvas = this.canvas.getContext('2d')
+    canvas.clearRect(0, 0, viewportWidth, viewportHeight)
+    for (let i = 0; i < this.dots.length; i++) {
+      for (let j = i + 1; j < this.dots.length; j++) {
+        const dot1 = this.dots[i]
+        const dot2 = this.dots[j]
+        const a = dot1.x - dot2.x
+        const b = dot1.y - dot2.y
+        const dist = Math.sqrt(a * a + b * b)
         const threshold = Math.min(viewportWidth, viewportHeight) / 6
         if (dist < threshold) {
-          pairs.push({
-            x1: dot1.x,
-            y1: dot1.y,
-            x2: dot2.x,
-            y2: dot2.y,
-            opacity: Math.min(threshold / dist - 1, 1),
-          })
+          const opacity = Math.min(threshold / dist - 1, 1)
+          this.drawLine(canvas, dot1.x, dot1.y, dot2.x, dot2.y, opacity)
         }
       }
     }
-    const canvas = this.canvas.getContext('2d')
-    canvas.clearRect(0, 0, viewportWidth, viewportHeight)
-    pairs.forEach(({ x1, y1, x2, y2, opacity }) => {
-      this.drawLine(canvas, x1, y1, x2, y2, opacity)
-    })
-    dots.forEach(({ x, y, radius, opacity }) => {
+    this.dots.forEach(({ x, y, radius, opacity }) => {
       this.drawDot(canvas, x, y, radius, opacity)
     })
-    this.setState({ dots, pairs })
   }
 
   generateNewDot() {
@@ -80,40 +74,39 @@ export default class Dots extends Component {
       clientHeight: viewportHeight,
     } = document.documentElement
     const randomPositive = (a, b) => a + (Math.random() * (b - a))
-    const random = (a, b) => Math.random() > 0.5
+    const randomNumber = (a, b) => Math.random() > 0.5
       ? randomPositive(a, b)
       : -randomPositive(a, b)
     const radius = randomPositive(3, 6)
     const opacity = randomPositive(0.5, 1)
     let x, y, dx, dy
-    if (Math.random() > 0.5) {
-      if (Math.random() > 0.5) {
-        // top
-        x = randomPositive(0, viewportWidth)
-        y = 0
-        dx = random(0.5, 1.5)
-        dy = randomPositive(0.5, 1.5)
-      } else {
-        // right
-        x = viewportWidth
-        y = randomPositive(0, viewportHeight)
-        dx = -randomPositive(0.5, 1.5)
-        dy = random(0.5, 1.5)
-      }
+    const random = Math.random()
+    const a = 0.5 / 3
+    const b = 0.5
+    if (random < 0.25) {
+      // top
+      x = randomPositive(0, viewportWidth)
+      y = 0
+      dx = randomNumber(a, b)
+      dy = randomPositive(a, b)
+    } else if (random < 0.5) {
+      // right
+      x = viewportWidth
+      y = randomPositive(0, viewportHeight)
+      dx = -randomPositive(a, b)
+      dy = randomNumber(a, b)
+    } else if (random < 0.75) {
+      // bottom
+      x = randomPositive(0, viewportWidth)
+      y = viewportHeight
+      dx = randomNumber(a, b)
+      dy = -randomPositive(a, b)
     } else {
-      if (Math.random() > 0.5) {
-        // bottom
-        x = randomPositive(0, viewportWidth)
-        y = viewportHeight
-        dx = random(0.5, 1.5)
-        dy = -randomPositive(0.5, 1.5)
-      } else {
-        // left
-        x = 0
-        y = randomPositive(0, viewportHeight)
-        dx = randomPositive(0.5, 1.5)
-        dy = random(0.5, 1.5)
-      }
+      // left
+      x = 0
+      y = randomPositive(0, viewportHeight)
+      dx = randomPositive(a, b)
+      dy = randomNumber(a, b)
     }
     return { x, y, dx, dy, radius, opacity }
   }

75ab167d03717ec5b6496646896901f0d89c3408
Tim Stoddard <tim.stoddard2@gmail.com>
2018-05-11 20:08:15 -0700

    update start page links

--- src/start/Links.jsx
+++ src/start/Links.jsx
@@ -101,7 +101,7 @@ const links = [
   },
   {
     name: 'Student Center',
-    href: 'https://my.calpoly.edu/cas/login?service=https://cmsweb.calpoly.edu/psp/HSLOPRD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HC_SSS_STUDENT_CENTER%26IsFolder=false%26IgnoreParamTempl=FolderPath%2cIsFolder%26RL=%26target=main0%26navc=2398?clg=login%26languageCd=ENG%26userid=PS%26pwd=z',
+    href: 'https://idp.calpoly.edu/idp/profile/cas/login?service=https://cmsweb.calpoly.edu/psp/CSLOPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HC_SSS_STUDENT_CENTER%26IsFolder=false%26IgnoreParamTempl=FolderPath%2cIsFolder%26RL=%26target=main0%26navc=2398?cmd=login%26languageCd=ENG%26userid=PS%26pwd=z&method=post',
   },
   {
     name: 'Poly Profile',
@@ -133,7 +133,7 @@ const links = [
   },
   {
     name: 'Cal Poly Links',
-    href: 'https://my.calpoly.edu/cas/limitedLayout',
+    href: 'https://myportalisdown-calpoly-edu.s3.amazonaws.com/main.html',
   },
 ]
 

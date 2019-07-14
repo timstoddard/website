ca8dbd5e23c08ccb0c8ac0f35df1715804fd037d
Tim Stoddard <tim.stoddard2@gmail.com>
2019-07-14 12:40:30 -0700

    remove all .DS_Store files before uploading to S3

--- upload.sh
+++ upload.sh
@@ -1,3 +1,6 @@
+# recursively delete all .DS_Store files
+find . -name '.DS_Store' -type f -delete
+
 # upload prod files to s3
 aws s3 cp index.html s3://timstoddard.me/index.html
 aws s3 cp dist s3://timstoddard.me/dist --recursive

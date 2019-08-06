f5e51d73ef97539fdf96e2bb29b401cb606d2882
Tim Stoddard <tim.stoddard2@gmail.com>
2019-08-06 09:49:51 -0700

    improve s3 upload script

--- upload.sh
+++ upload.sh
@@ -2,6 +2,13 @@
 find . -name '.DS_Store' -type f -delete
 
 # upload prod files to s3
+
+# index html
 aws s3 cp index.html s3://timstoddard.me/index.html
+
+# remove old built files before uploading new ones
+aws s3 rm s3://timstoddard.me/dist --recursive
 aws s3 cp dist s3://timstoddard.me/dist --recursive
-aws s3 cp media s3://timstoddard.me/media --recursive
+
+# only upload new/modified media files
+aws s3 sync media s3://timstoddard.me/media

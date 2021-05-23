83c2f9caf76dc1e1038a5f015ead9298611f84f1
Tim Stoddard <tim.stoddard2@gmail.com>
2021-05-22 21:43:54 -0700

    update to latest npm package versions

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,16 @@
 const common = require('./webpack.common')
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin')
 
 module.exports = common.merge(common.config('dev'), {
   mode: 'development',
   devtool: 'eval', // fastest page reload time
-  plugins: common.sharedPlugins('dev'),
+  plugins: [
+    ...common.sharedPlugins('dev'),
+    new ForkTsCheckerNotifierWebpackPlugin({
+      title: 'TypeScript',
+      excludeWarnings: false,
+    }),
+  ],
   devServer: {
     // contentBase: './dist',
     historyApiFallback: true,
